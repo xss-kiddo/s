954ccbb93050089c3d3f00c65ab17ea0bfd70142
xss-kiddo <50673970+xss-kiddo@users.noreply.github.com>
2019-05-15 20:24:31 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,3 @@
-document.body.style.opacity=0;for(let i=0;i<1/0;i++)document.location.href="#",window.history.back(),window.history.forward(),console.log('xss');;
+document.body.style.opacity=0;let txt='a';setTimeout(function(){console.log('buna draga; e gata site-ul?');txt+=txt},5);
 // protect against xss, kiddo =)))
 // https://youtu.be/bNVgQBHHl4I 
